fa4b3197da8fece78d5fac42130080d25db30d01
thisisbatra <batranitin118@gmail.com>
2025-01-08 20:22:55 +0530

    made code readable

--- src/oops/inheritance/Animal.java
+++ src/oops/inheritance/Animal.java
@@ -1,6 +1,7 @@
 package oops.inheritance;
 
 public class Animal {
+    //here animal class has two methods one of them is static
     void sound(){
         System.out.println("i have some sound!");
     }
@@ -9,6 +10,9 @@ public class Animal {
     }
 }
 class Cat extends Animal{
+    //after inherited animal class we can access the data members as well as data methods inside child scope.
+    //methods inherited will be depend on what type of modifier is used.
+    //here sound method is overrided as both parent and child has same method which share same signature.
     void sound(){
         System.out.println("people says i meow");
     }
@@ -23,6 +27,4 @@ class Dog extends Animal{
     public static void flexible(){
         System.out.println("i am less flexible than cat");
     }
-}
-class Lion extends Animal{
 }
--- src/oops/inheritance/Driver.java
+++ src/oops/inheritance/Driver.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 public class Driver {
     public static void main(String args[]){
-        Cat pussy=new Cat();
-        Lion sher=new Lion();
-        pussy.sound();
-//        Cat.flexible();
-        pussy.flexible();
-        Animal a1=new Cat();
-        a1.flexible();
+//        Cat pussy=new Cat();
+//        pussy.sound();
+//        pussy.flexible();
+//        Animal a1=new Cat();
+//        a1.flexible();
     }
 }
